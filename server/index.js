c7b9754b02b73f45ec4f6ac4cf7bf0b253d35202
armouti <alarmouti.ismail@gmail.com>
2018-11-15 17:43:25 -0600

    added missing require statement for dependency 'path' in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Listing = require('../database/Listing.js');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 1128;
