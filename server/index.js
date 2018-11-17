0a143729a9e49d65cbc880af72465e44871ef118
armouti <alarmouti.ismail@gmail.com>
2018-11-17 15:39:27 -0600

    completed extending api endpoints with all crud operations using the db and orm previously utilized in the codebase

--- server/index.js
+++ server/index.js
@@ -19,25 +19,26 @@ app.get('/listings', (req, res) => {
 });
 
 app.get('/listings/:id', (req, res) => {
-  Listing.find({photoId: req.params.id}, (err, listing) => {
-    if (err) {
-      throw err;
-    } else {
-      return res.send(listing)
-    }
+  Listing.find({id: req.params.id}, (err, listing) => {
+    if (err) console.log('Error GET/ listings/:id', err)
+    else res.send(listing)
   })
 })
 
 app.delete('/listings/:id', (req, res) => {
+  console.log('delete fired, params:', req.params.id)
   Listing.deleteOne({id: req.params.id}, (err) => {
-    if (err) console.log(err);
+    if (err) console.log('error DELETE /listings/:id', err);
+    else res.send('listing successfully deleted');
   })
 });
 
 app.put('/listings/:id', (req, res) => {
-  Listing.updateOne({}, (err) => {
+  Listing.updateOne(req.body.listing, (err) => {
     if (err) console.log(err);
+    else res.send('listing successfully updated')
   })
+  res.send('completed');
 });
 
 app.post('/listings', (req, res) => {
